62f08d827596720f77df58976861015d816a2eec
MungC2010G2 <zhaiduong26z@gmail.com>
2020-12-09 17:04:49 +0700

    add file hien thi loi chao

---
+++ src/HienThiLoiChao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Moi ban nhap ten vao:");
+        String name = s.nextLine();
+        System.out.println("Hello: " + name );
+    }
+}
